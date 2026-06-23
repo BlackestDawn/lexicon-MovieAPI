cc5423979a5d406c2bc51633e3a0b9a71ead12a5
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-23 10:33:32 +0200

    switching Movie to error middleware

--- src/MovieAPI.Application/Interfaces/IMovieService.cs
+++ src/MovieAPI.Application/Interfaces/IMovieService.cs
@@ -7,9 +7,9 @@ namespace MovieAPI.Application.Interfaces;
 public interface IMovieService
 {
   Task<(IEnumerable<MovieDto>, PaginationMetadata?)> GetMany(MovieSearchParams searchParams, int? page, int? pageSize, CancellationToken token = default);
-  Task<MovieExtendedDto?> GetOne(Guid id, bool includePeople = false, CancellationToken token = default);
-  Task<MovieCreationResult> Create(MovieForChangeDto newMovie, CancellationToken token = default);
-  Task<(bool, string?)> Update(Guid id, MovieForChangeDto updatedMovie, CancellationToken token = default);
-  Task<(bool, string?)> Update(Guid id, JsonPatchDocument<MovieForChangeDto> patchDocument, CancellationToken token = default);
+  Task<MovieExtendedDto> GetOne(Guid id, bool includePeople = false, CancellationToken token = default);
+  Task<MovieDto> Create(MovieForChangeDto newMovie, CancellationToken token = default);
+  Task Update(Guid id, MovieForChangeDto updatedMovie, CancellationToken token = default);
+  Task Update(Guid id, JsonPatchDocument<MovieForChangeDto> patchDocument, CancellationToken token = default);
   Task Remove(Guid id, CancellationToken token = default);
 }
