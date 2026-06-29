bcf6d8f37c90dc9802b73355e855370c53c8af3c
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-29 12:40:33 +0200

    adding XML comments to genres controller

--- src/MovieAPI.Api/Controllers/GenresController.cs
+++ src/MovieAPI.Api/Controllers/GenresController.cs
@@ -9,12 +9,20 @@ using MovieAPI.Domain.Constants;
 
 namespace MovieAPI.Api.Controllers;
 
+/// <summary>
+/// Controller for handling genres
+/// </summary>
 [ApiController]
 [Route("api/v{version:apiVersion}/genres")]
 [ApiVersion("1.0")]
 [ApiVersion("2.0")]
 public class GenresController(IGenreService service, IOutputCacheStore cacheStore) : ControllerBase
 {
+  /// <summary>
+  /// Fetch list of all genres
+  /// </summary>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>List of GenreDto objects</returns>
   [HttpGet]
   [OutputCache(PolicyName = "CatalogCache")]
   public async Task<IActionResult> GetGenres(CancellationToken cancellationToken = default)
@@ -23,6 +31,13 @@ public class GenresController(IGenreService service, IOutputCacheStore cacheStor
     return Ok(result);
   }
 
+  /// <summary>
+  /// Get single genre with optionally a list of movies for that genre
+  /// </summary>
+  /// <param name="id">GUID of genre</param>
+  /// <param name="includeMovies">if to include movies</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>GenreExtendedDto object</returns>
   [HttpGet("{id}", Name = "GetGenre")]
   [OutputCache(PolicyName = "CatalogCache")]
   public async Task<IActionResult> GetGenre(Guid id, bool includeMovies = true, CancellationToken cancellationToken = default)
@@ -31,6 +46,12 @@ public class GenresController(IGenreService service, IOutputCacheStore cacheStor
     return Ok(result);
   }
 
+  /// <summary>
+  /// Create new genre, only available to moderators and above
+  /// </summary>
+  /// <param name="newGenre">GenreForChangeDto object</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>GenreDto object with route to it</returns>
   [Authorize(Roles = Roles.ModeratorAndAbove)]
   [HttpPost]
   public async Task<IActionResult> CreateGenre(GenreForChangeDto newGenre, CancellationToken cancellationToken = default)
@@ -40,6 +61,13 @@ public class GenresController(IGenreService service, IOutputCacheStore cacheStor
     return CreatedAtRoute("GetGenre", new { result.Id }, result);
   }
 
+  /// <summary>
+  /// Full object update of existing genre, only available to moderators and above
+  /// </summary>
+  /// <param name="id">GUID of genre to update</param>
+  /// <param name="updatedGenre">GenreForChangeDto object</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>HTTP code: 204</returns>
   [Authorize(Roles = Roles.ModeratorAndAbove)]
   [HttpPut("{id}")]
   public async Task<IActionResult> UpdateGenre(Guid id, GenreForChangeDto updatedGenre, CancellationToken cancellationToken = default)
@@ -49,6 +77,13 @@ public class GenresController(IGenreService service, IOutputCacheStore cacheStor
     return NoContent();
   }
 
+  /// <summary>
+  /// Update existing genre through JSON patch, only available to moderators and above
+  /// </summary>
+  /// <param name="id">GUID of genre to update</param>
+  /// <param name="patch">JSON patch document of changes</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>HTTP code: 204</returns>
   [Authorize(Roles = Roles.ModeratorAndAbove)]
   [HttpPatch("{id}")]
   public async Task<IActionResult> PatchGenre(Guid id, JsonPatchDocument<GenreForChangeDto> patch, CancellationToken cancellationToken = default)
@@ -58,6 +93,12 @@ public class GenresController(IGenreService service, IOutputCacheStore cacheStor
     return NoContent();
   }
 
+  /// <summary>
+  /// Remove a genre, only available to administrators
+  /// </summary>
+  /// <param name="id">GUID of genre to remove</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>HTTP code: 204</returns>
   [Authorize(Roles = Roles.Administrator)]
   [HttpDelete("{id}")]
   public async Task<IActionResult> DeleteGenre(Guid id, CancellationToken cancellationToken = default)
