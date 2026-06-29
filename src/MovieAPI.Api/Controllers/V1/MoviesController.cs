4f4773f0fd02fbb7fbd7aa4d584c86806f0642e0
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-29 13:24:39 +0200

    adding XML comments to movies controllers

--- src/MovieAPI.Api/Controllers/V1/MoviesController.cs
+++ src/MovieAPI.Api/Controllers/V1/MoviesController.cs
@@ -13,6 +13,9 @@ using MovieAPI.Infrastructure.Models;
 
 namespace MovieAPI.Api.Controllers.V1;
 
+/// <summary>
+/// V1 Controller for handling movies
+/// </summary>
 [ApiController]
 [Route("api/v{version:apiVersion}/movies")]
 [ApiVersion("1.0")]
@@ -21,6 +24,18 @@ public class MoviesController(
   IOutputCacheStore cacheStore,
   IMapper mapper) : ControllerBase
 {
+  /// <summary>
+  /// Fetch paginated and filterable list of movies
+  /// </summary>
+  /// <param name="name">Filter on movie title</param>
+  /// <param name="search">Search in synopsis</param>
+  /// <param name="genre">Filter on genre</param>
+  /// <param name="year">Filter on release year</param>
+  /// <param name="minRating">Filter on minimum rating</param>
+  /// <param name="page">page to display, defaults to 1</param>
+  /// <param name="pageSize">Amount per page, defaults to 10</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>List of MovieDto objects</returns>
   [HttpGet]
   [OutputCache(PolicyName = "CatalogCache")]
   public async Task<IActionResult> GetMovies(string? name, string? search, string? genre,
@@ -40,6 +55,13 @@ public class MoviesController(
     return Ok(result);
   }
 
+  /// <summary>
+  /// Get specific movie with extended information
+  /// </summary>
+  /// <param name="id">GUID of movie</param>
+  /// <param name="includePeople">If to include persons participating in movie</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>MovieExtendedV1Dto object</returns>
   [HttpGet("{id}", Name = "GetMovie")]
   [OutputCache(PolicyName = "CatalogCache")]
   public async Task<IActionResult> GetMovie(Guid id, bool includePeople = true,
@@ -49,6 +71,12 @@ public class MoviesController(
     return Ok(mapper.Map<MovieExtendedV1Dto>(result));
   }
 
+  /// <summary>
+  /// Create new movie, needs power user or above
+  /// </summary>
+  /// <param name="newMovie">MovieForChangeDto object</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>MovieDto object and route to said object</returns>
   [Authorize(Roles = Roles.PowerUserAndAbove)]
   [HttpPost]
   public async Task<IActionResult> CreateMovie(MovieForChangeDto newMovie,
@@ -59,6 +87,13 @@ public class MoviesController(
     return CreatedAtRoute("GetMovie", new { result.Id }, result);
   }
 
+  /// <summary>
+  /// Whole object update of a movie, needs power user or above
+  /// </summary>
+  /// <param name="id">GUID of movie</param>
+  /// <param name="updatedMovie">MovieForChangeDto object</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>HTTP code: 204</returns>
   [Authorize(Roles = Roles.PowerUserAndAbove)]
   [HttpPut("{id}")]
   public async Task<IActionResult> UpdateMovie(Guid id, MovieForChangeDto updatedMovie,
@@ -69,6 +104,13 @@ public class MoviesController(
     return NoContent();
   }
 
+  /// <summary>
+  /// Update movie through JSON patch, needs power user or above
+  /// </summary>
+  /// <param name="id">GUID of movie</param>
+  /// <param name="patch">JSON patch document</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>HTTP code: 204</returns>
   [Authorize(Roles = Roles.PowerUserAndAbove)]
   [HttpPatch("{id}")]
   public async Task<IActionResult> PatchMovie(Guid id, JsonPatchDocument<MovieForChangeDto> patch,
@@ -79,6 +121,12 @@ public class MoviesController(
     return NoContent();
   }
 
+  /// <summary>
+  /// Remove a movie
+  /// </summary>
+  /// <param name="id">GUID of movie</param>
+  /// <param name="cancellationToken">Notification token for canceling operations</param>
+  /// <returns>HTTP code: 204</returns>
   [Authorize(Roles = Roles.ModeratorAndAbove)]
   [HttpDelete("{id}")]
   public async Task<IActionResult> RemoveMovie(Guid id,
