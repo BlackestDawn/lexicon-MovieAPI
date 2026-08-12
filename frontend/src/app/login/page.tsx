f032c3e6595567fcc5452bd8afd0d66185a226a5
Alexander Stauch <blacke4dawn@gmail.com>
2026-08-12 10:04:40 +0200

    adding Lucide-React package

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { BaseContainer } from "@/components/base/baseContainer";
+
+export default function Login() {
+  return (
+    <BaseContainer>
+      <div>
+
+      </div>
+    </BaseContainer>
+  )
+}
