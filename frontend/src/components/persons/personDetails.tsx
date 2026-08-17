8b99de145a5a2d65d21e2fa52b9ee465dc216c7c
Alexander Stauch <blacke4dawn@gmail.com>
2026-08-17 10:56:22 +0200

    adding delete button to persons details page

--- frontend/src/components/persons/personDetails.tsx
+++ frontend/src/components/persons/personDetails.tsx
@@ -1,8 +1,9 @@
-import { getPerson } from "@/lib/actions/person";
+import { getPerson, removePerson } from "@/lib/actions/person";
 import { notFound } from "next/navigation";
 import RestrictedComponent from "../auth/restrictedComponent";
 import Link from "next/link";
 import { personRoleLabels } from "@/lib/data/models/personRoleTypes";
+import SimpleDeleteButton from "../general/buttons/simpleDeleteButton";
 
 export default async function PersonDetails({ id }: { id: string }) {
   const person = await getPerson(id);
@@ -10,7 +11,7 @@ export default async function PersonDetails({ id }: { id: string }) {
 
   return (
     <div className="w-full m-4 space-y-6">
-      <div className="flex flex-between">
+      <div className="flex justify-between">
         <h3>
           {person.givenName} {person.middleName ? ` ${person.middleName}` : ""}
           {person.lastName}
@@ -20,7 +21,11 @@ export default async function PersonDetails({ id }: { id: string }) {
             <div></div> {/** TODO: add edit button */}
           </RestrictedComponent>
           <RestrictedComponent accessLevel="ModeratorAndAbove">
-            <div></div> {/** TODO: add delete button */}
+            <SimpleDeleteButton
+              id={person.id}
+              redirectTo="/persons"
+              onDelete={removePerson}
+            />
           </RestrictedComponent>
         </div>
       </div>
@@ -28,10 +33,12 @@ export default async function PersonDetails({ id }: { id: string }) {
         <p>Date of birth: {person.dateOfBirth.toDateString()}</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-        {person.movieRoles.map(mr => (
+        {person.movieRoles.map((mr) => (
           <Link key={mr.movieId} href={`/movies/${mr.movieId}`}>
             <div className="p-4 border border-slate-600 dark:border-slate-400 rounded-md text-center">
-              <p>{mr.title}: {personRoleLabels[mr.role]}</p>
+              <p>
+                {mr.title}: {personRoleLabels[mr.role]}
+              </p>
             </div>
           </Link>
         ))}
